06f9280a7d11e4028d5922d204fa5fcb7490e611
Genbel <Genbel@MacBook-Pro.local>
2016-03-18 17:40:40 +0000

    Section 3

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './courses.component', './authors.component'], function(exports_1, context_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,30 +10,37 @@ System.register(['angular2/core', './courses.component', './authors.component'],
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1, authors_component_1;
+    var core_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (courses_component_1_1) {
-                courses_component_1 = courses_component_1_1;
-            },
-            function (authors_component_1_1) {
-                authors_component_1 = authors_component_1_1;
             }],
         execute: function() {
             // This it calls in Type Script decorator and is part of the class
             AppComponent = (function () {
                 function AppComponent() {
+                    this.isActive = true;
+                    this.token = "Lina";
+                    // what we get on the above line this simplify that action with ngmodel
+                    this.twoWayDataBinding = "Hi Linute!";
+                    this.oneWay = "Hi Oier!";
                 }
+                AppComponent.prototype.clickEvent = function ($event) {
+                    console.log('Click');
+                };
+                AppComponent.prototype.hoverEvent = function () {
+                    console.log('Hover');
+                };
+                AppComponent.prototype.showTypeText = function (text) {
+                    console.log("Typed text: " + text);
+                    console.log("Two way data binding text: " + this.twoWayDataBinding);
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "<h1>Hello Angular 2</h1>\n                <courses></courses>\n                <authors></authors>",
-                        // We specify the components or directive that we've used inside of the template for this component
-                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent]
+                        template: "\n                <button class=\"btn btn-primary\" [class.hello]=\"isActive\" [class.active]=\"isActive\" [attr.data-id]=\"token\" [id]=\"token\" (click)=\"clickEvent($event)\">Class binding</button>\n                <button class=\"btn btn-warning\" [style.color]=\"'black'\" [style.border-color]=\"isActive ? 'Black' : 'White'\" (mouseover)=\"hoverEvent()\">Style binding</button>\n                <input type=\"text\" [value]=\"oneWay\"/><input type=\"text\" (input)=\"showTypeText($event.target.value)\" />\n                <input type=\"text\" [(ngModel)]=\"twoWayDataBinding\" />\n                <h1>{{title}}</h1>\n                <div><i class=\"glyphicon\" [class.glyphicon-star]=\"isActive\" [class.glyphicon-star-empty]=\"!isActive\" (click)=\"isActive= !isActive\"></i></div> \n         \n    "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
@@ -43,4 +50,21 @@ System.register(['angular2/core', './courses.component', './authors.component'],
         }
     }
 });
+/* ANGULAR JUMP START LESSON
+
+import {Component} from 'angular2/core'
+import {CoursesComponent} from './courses.component'
+import {AuthorsComponent} from './authors.component'
+
+// This it calls in Type Script decorator and is part of the class
+@Component({
+    selector: 'my-app',
+    template: `<h1>Hello Angular 2</h1>
+                <courses></courses>
+                <authors></authors>`,
+    // We specify the components or directive that we've used inside of the template for this component
+    directives: [CoursesComponent, AuthorsComponent]
+})
+export class AppComponent {
+}*/ 
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,43 @@
 import {Component} from 'angular2/core'
+
+// This it calls in Type Script decorator and is part of the class
+@Component({
+    selector: 'my-app',
+    template: `
+                <button class="btn btn-primary" [class.hello]="isActive" [class.active]="isActive" [attr.data-id]="token" [id]="token" (click)="clickEvent($event)">Class binding</button>
+                <button class="btn btn-warning" [style.color]="'black'" [style.border-color]="isActive ? 'Black' : 'White'" (mouseover)="hoverEvent()">Style binding</button>
+                <input type="text" [value]="oneWay"/><input type="text" (input)="showTypeText($event.target.value)" />
+                <input type="text" [(ngModel)]="twoWayDataBinding" />
+                <h1>{{title}}</h1>
+                <div><i class="glyphicon" [class.glyphicon-star]="isActive" [class.glyphicon-star-empty]="!isActive" (click)="isActive= !isActive"></i></div> 
+         
+    `
+})
+export class AppComponent {
+    isActive = true;
+    token = "Lina";
+    // what we get on the above line this simplify that action with ngmodel
+    twoWayDataBinding = "Hi Linute!";
+    oneWay = "Hi Oier!"; 
+    
+    clickEvent($event){
+        console.log('Click');
+    }
+    
+    hoverEvent(){
+        console.log('Hover')
+    }
+    
+    showTypeText(text){
+        console.log("Typed text: " + text);
+        console.log("Two way data binding text: " + this.twoWayDataBinding);
+    }
+    
+}
+
+/* ANGULAR JUMP START LESSON
+
+import {Component} from 'angular2/core'
 import {CoursesComponent} from './courses.component'
 import {AuthorsComponent} from './authors.component'
 
@@ -12,4 +51,4 @@ import {AuthorsComponent} from './authors.component'
     directives: [CoursesComponent, AuthorsComponent]
 })
 export class AppComponent {
-}
+}*/
